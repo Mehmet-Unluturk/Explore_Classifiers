01386ca6975b61ad81dc728ccfe25cb4f1116ade
Mehmet-Unluturk <unluturkmehmet0@gmail.com>
2023-09-24 17:13:38 +0300

    Add data visualization and two new classifiers Add scatter plot Add GaussianNaive and Multinomial Naive Bayes Classifiers

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 from sklearn import datasets
 import numpy as np
+import pandas as pd
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 st.title("Project X")
 
@@ -15,17 +21,15 @@ st.write("""
 *Try Parameters*
 """)
 
-dataset_name= st.sidebar.selectbox("Select Dataset", ("Iris","Wine","Breast Cancer","Digits"))
+dataset_name= st.sidebar.selectbox("Select Dataset", ("Iris","Wine","Breast Cancer"))
 
-classifier_name= st.sidebar.selectbox("Select Classifier", ("KNN","SVM","Random Forest"))
+classifier_name= st.sidebar.selectbox("Select Classifier", ("KNN","SVM","Random Forest","GaussianNB","MultinomialNB"))
 
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
     elif dataset_name == "Breast Cancer":
         data = datasets.load_breast_cancer()
-    elif dataset_name == "Digits":
-        data =datasets.load_digits()
     else:
         data = datasets.load_wine()
     X = data.data
@@ -44,8 +48,16 @@ def add_parameter(clf_name):
     elif clf_name == "SVM":
         C = st.sidebar.slider("C", 0.01, 10.0)
         params["C"] = C
+    elif clf_name == "GaussianNB":
+        st.sidebar.write("The Gaussian Naive Bayes (GaussianNB) classifier makes predictions by calculating the probabilities between classes in the data set. The basic functionality of this model is implemented automatically using the properties and class labels of the dataset and does not contain special parameters that need to be set by the user.")
+    elif clf_name == "MultinomialNB":
+        alpha = st.sidebar.slider("alpha", 0.0, 1.0, 0.5)
+        fit_prior = st.sidebar.checkbox("fit_prior", value=True)
+    
+        params["alpha"] = alpha
+        params["fit_prior"] = fit_prior
     else:
-        max_depth = st.sidebar.slider("max_depth", 2,15)
+        max_depth = st.sidebar.slider("max_depth", 2, 15)
         n_estimators = st.sidebar.slider("n_estimators", 1, 100)
         params["n_estimators"] = n_estimators
         params["max_depth"] = max_depth
@@ -58,6 +70,11 @@ def get_classifier(clf_name,params):
         clf = KNeighborsClassifier(n_neighbors=params["K"])
     elif clf_name == "SVM":
         clf = SVC(C=params["C"])
+    elif clf_name == "GaussianNB":
+        clf = GaussianNB()
+    elif clf_name == "MultinomialNB":
+        clf = MultinomialNB(alpha=params["alpha"], 
+                            fit_prior=params["fit_prior"])
     else:
         clf = RandomForestClassifier(n_estimators=params["n_estimators"],
                                      max_depth=params["max_depth"],random_state=1234)
@@ -75,3 +92,20 @@ y_pred = clf.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 st.write(f"classifier={classifier_name}")
 st.write(f"accuracy= {acc}")
+
+#Plotting
+pca =PCA(2)
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig, ax = plt.subplots()
+scatter = ax.scatter(x1, x2, c=y, alpha=0.8, cmap="viridis")
+ax.set_xlabel("Principal Component 1")
+ax.set_ylabel("Principal Component 2")
+fig.colorbar(scatter)
+
+st.pyplot(fig)
+
+
